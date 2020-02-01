0955a40e1e9c9fda9cb0369460667ade965e8171
cancan-huang <ruc_cancan@ruc.edu.cn>
2020-02-01 11:53:46 +0000

    Update day3.py

--- day3.py
+++ day3.py
@@ -1,6 +1,7 @@
 # day 3
 
 # practice to transform 12345 into 54321
+# my answer
 
 number = int(input('Please enter the number:'))
 b = len(str(number))
@@ -9,3 +10,14 @@ for i in range(b):
     get_number = ( number // (10 ** i) ) % 10
     result += get_number * (10 ** (b - i - 1))
 print(result)
+
+
+# case answer
+num = int(input('num = '))
+reversed_num = 0
+while num > 0:
+    reversed_num = reversed_num * 10 + num % 10
+    #print(reversed_num)
+    num //= 10
+    #print(num)
+print(reversed_num)
